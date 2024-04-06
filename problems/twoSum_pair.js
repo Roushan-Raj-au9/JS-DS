03881a66dbc513bdbe783e70efc9c2f10cc00689
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2024-04-07 00:30:47 +0530

    Updated two sum pair solution

--- problems/twoSum_pair.js
+++ problems/twoSum_pair.js
@@ -1,5 +1,7 @@
-let lst = [2, 1, 7, 11, 3]
-let target = 4;
+// let lst = [2, 1, 7, 11, 3]
+// let target = 4;
+let lst = [3,3]
+let target = 6;
 
 let result = [];
 
@@ -16,8 +18,20 @@ let result = [];
 //     }
 // }
 
+
+// // not full accurate
+// for(let i = 0; i < lst.length; i++){
+//     if(target - lst[i] < lst[i] && lst.includes(target - lst[i])){
+//         console.log("sum pairs >> ", lst.indexOf(target - lst[i]), lst.indexOf(lst[i]))
+//     }
+// }
+
+
+//full accurate
+let map = new Map();
 for(let i = 0; i < lst.length; i++){
-    if(target - lst[i] < lst[i] && lst.includes(target - lst[i])){
-        console.log("sum pairs >> ", lst.indexOf(target - lst[i]), lst.indexOf(lst[i]))
+    if(map.has(target - lst[i])){
+        console.log("sum pairs >>> ", [map.get(target - lst[i]), i])
     }
+    map.set(lst[i], i)
 }
