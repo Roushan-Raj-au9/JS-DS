0750234cc5c56573f3e707a25d4ff4e43c5a2fad
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2024-04-08 14:45:46 +0530

    Call, bind, apply

---
+++ JS/methods.js
@@ -0,0 +1,34 @@
+// //Call method
+// function greet(message){
+//     console.log("Hi " + this.name + ' ' + message)
+// }
+
+// const person = {
+//     name: "Jhon"
+// }
+
+// greet.call(person, 'Good morning');
+
+
+// //apply method
+// function greet(message, message2){
+//     console.log("Hi " + this.name + ' ' + message + ' ' + message2)
+// }
+
+// const person = {
+//     name: "Raj"
+// }
+
+// greet.apply(person, ['Good Morning', 'See You'])
+
+// Bind method
+function greet(message){
+    console.log("Hi " + this.name + ' ' + message)
+}
+
+const person = {
+    name: "Mike"
+}
+
+const greetPerson = greet.bind(person, 'Good Morning')
+greetPerson()
