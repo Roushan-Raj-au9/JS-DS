fdeaeef380dfc6256c68142bc6c9033ca8fd346b
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2024-04-13 17:19:09 +0530

    Promise chaining

--- JS/promiseQue.js
+++ JS/promiseQue.js
@@ -6,4 +6,20 @@ const get = async() => {
 
 get()
 .then((data) => console.log("data >> ", data))
-.catch((err) => console.log("error >> ", err))
+.catch((err) => console.log("error >> ", err));
+
+
+const promisifiedFunc = (duration) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("resolved promisifiedFunc data 1")
+        }, duration)
+    })
+};
+
+promisifiedFunc(1000)
+.then((data) => {
+    console.log("promisifiedFunc data 1 >>> ", data)
+    promisifiedFunc(2000).then((data2) =>  console.log("promisifiedFunc data 2 ... ", data2))
+})
+.catch((err) => console.log("promisifiedFunc error >>> ", err))
