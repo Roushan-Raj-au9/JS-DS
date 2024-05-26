4e7b08b64d631fc0f128d16f8f51560ed63dfee2
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2024-05-26 17:26:41 +0530

    TDZ

--- JS/Callback.js
+++ JS/Callback.js
@@ -1,4 +1,8 @@
 // when we pass a function as a argument to other function is know as Callback;
+// or we can say the function we pass as an argument is known as callbacks
+
+// when a function accepts other function as an argument  is known as HOF (higher order function)
+// or we can say HOF are those function, which takes function as an argument
 
 // function sum(a, b) {
 //     let result = a + b;
