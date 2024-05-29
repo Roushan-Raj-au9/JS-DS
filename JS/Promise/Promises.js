03e0d13057fc6d810d3a2cefa978e4014c427ff0
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2024-05-29 15:08:48 +0530

    Iterators

---
+++ JS/Promise/Promises.js
@@ -0,0 +1,7 @@
+// # Enhanced Readability  
+// # Resolve IOC (Inversion Of Control)  ---> how can we ensure that the callbacks are called, we give control of execution
+
+//? In Js Promises are special types of objects that get returned immediately whwn we call them.
+
+//? Promises acts as a placeholder for the data, we hope to get back sometime in the future.
+
